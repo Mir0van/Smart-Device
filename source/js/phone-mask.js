a94a7bfc9365a3af6870ec7a71ffd479400322a2
Vladimir <vokar90@yandex.ru>
2023-03-23 21:23:11 +0300

    fix after test

--- source/js/phone-mask.js
+++ source/js/phone-mask.js
@@ -1,8 +1,11 @@
 import {initPhoneInput} from './modules/form-validate/init-phone-input.js';
 
 const activateTelMask = () => {
-  const inputTelParent = document.querySelector('.form__input-tel');
-  initPhoneInput(inputTelParent);
+  const inputTelParents = document.querySelectorAll('.form__input-tel');
+
+  inputTelParents.forEach((inputTelParent) => {
+    initPhoneInput(inputTelParent);
+  });
 };
 
 export {activateTelMask};
--- source/js/show-more.js
+++ source/js/show-more.js
@@ -1,6 +1,8 @@
 const showMoreContent = document.querySelector('.about');
 const showMoreButton = showMoreContent.querySelector('.about__button');
 const hiddenText = showMoreContent.querySelector('.about__additional-text');
+const hiddenTextMobile = showMoreContent.querySelector('.about__text-hidden');
+const mediaQuery = window.matchMedia('(max-width: 767px)');
 
 const activateShowMore = () => {
   showMoreButton.addEventListener('click', () => {
@@ -11,7 +13,12 @@ const activateShowMore = () => {
       hiddenText.style.height = `${hiddenText.scrollHeight}px`;
     } else {
       showMoreButton.textContent = 'Подробнее';
+      hiddenTextMobile.style.display = null;
       hiddenText.style.height = '0px';
+    }
+
+    if (showMoreButton.classList.contains('is-open') && mediaQuery.matches) {
+      hiddenTextMobile.style.display = 'inline';
     }
   });
 };
